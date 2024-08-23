355458f90f8fa5720361e7b74c2abd2c795fc1e9
AchmadIzaaz <myachmadizaaz@gmail.com>
2024-08-23 18:16:05 +0700

    Fix version and copyright

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,13 +1,11 @@
 <footer class="footer">
     <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-6">
-                <script>document.write(new Date().getFullYear())</script> © {{ $option['site-title']->value ?? config('app.name', 'Laravel') }}.
+        <div class="d-flex justify-content-between">
+            <div class="fst-italic small">
+                Version 1.0
             </div>
-            <div class="col-sm-6">
-                <div class="text-sm-end d-none d-sm-block">
-                    versi 0.0.001
-                </div>
+            <div>
+                <script>document.write(new Date().getFullYear())</script> © {{ $option['site-title']->value ?? config('app.name', 'Laravel') }}.
             </div>
         </div>
     </div>

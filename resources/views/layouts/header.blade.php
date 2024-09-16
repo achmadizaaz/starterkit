68a72d5c4e1d800bc7dc8a862ce290834da440f0
AchmadIzaaz <myachmadizaaz@gmail.com>
2024-09-16 13:29:11 +0700

    Fix search dan dark mode

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -19,12 +19,12 @@
             </button>
 
             <!-- App Search-->
-            {{-- <form class="app-search d-none d-lg-block">
+            <form class="app-search d-none d-lg-block">
                 <div class="position-relative">
-                    <input type="text" class="form-control" placeholder="Search...">
+                    <input type="text" class="form-control" placeholder="Search..." name="search" value="{{ request('search') }}">
                     <button class="btn btn-primary" type="button"><i class="bx bx-search-alt align-middle"></i></button>
                 </div>
-            </form> --}}
+            </form>
         </div>
 
         <div class="d-flex">
@@ -40,7 +40,7 @@
                     <form class="p-3">
                         <div class="form-group m-0">
                             <div class="input-group">
-                                <input type="text" class="form-control" placeholder="Search ..." aria-label="Search Result">
+                                <input type="text" class="form-control" placeholder="Search ..." aria-label="Search Result" name="search" value="{{ request('search') }}">
 
                                 <button class="btn btn-primary" type="submit"><i class="mdi mdi-magnify"></i></button>
                             </div>
@@ -51,7 +51,7 @@
 
 
             <!-- Mode themes  -->
-            <div class="dropdown d-none d-sm-inline-block bd-mode-toggle">
+            <div class="dropdown bd-mode-toggle">
                 <button class="btn btn-bd-primary py-2 dropdown-toggle header-item" id="bd-theme" type="button" aria-expanded="false" data-bs-toggle="dropdown" aria-label="Toggle theme (auto)">
                     <span class="bi my-1 theme-icon-active">
                         <i class="theme-change-icon"></i>
@@ -139,7 +139,7 @@
                 <button type="button" class="btn header-item noti-icon position-relative" id="page-header-notifications-dropdown"
                 data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <i data-feather="bell" class="icon-lg"></i>
-                    <span class="badge bg-danger rounded-pill">5</span>
+                    <span class="badge bg-danger rounded-pill">99+</span>
                 </button>
                 <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end p-0"
                     aria-labelledby="page-header-notifications-dropdown">
@@ -149,25 +149,12 @@
                                 <h6 class="m-0"> Notifications </h6>
                             </div>
                             <div class="col-auto">
-                                <a href="#!" class="small text-reset text-decoration-underline"> Unread (3)</a>
+                                <a href="#!" class="small text-reset text-decoration-underline"> Unread (2)</a>
                             </div>
                         </div>
                     </div>
                     <div data-simplebar style="max-height: 230px;">
-                        <a href="#!" class="text-reset notification-item">
-                            <div class="d-flex">
-                                <div class="flex-shrink-0 me-3">
-                                    <img src="{{ asset('assets/images/users/avatar-3.jpg') }}" class="rounded-circle avatar-sm" alt="user-pic">
-                                </div>
-                                <div class="flex-grow-1">
-                                    <h6 class="mb-1">James Lemire</h6>
-                                    <div class="font-size-13 text-muted">
-                                        <p class="mb-1">It will seem like simplified English.</p>
-                                        <p class="mb-0"><i class="mdi mdi-clock-outline"></i> <span>1 hour ago</span></p>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
+                        
                         <a href="#!" class="text-reset notification-item">
                             <div class="d-flex">
                                 <div class="flex-shrink-0 avatar-sm me-3">
@@ -178,8 +165,8 @@
                                 <div class="flex-grow-1">
                                     <h6 class="mb-1">Your order is placed</h6>
                                     <div class="font-size-13 text-muted">
-                                        <p class="mb-1">If several languages coalesce the grammar</p>
-                                        <p class="mb-0"><i class="mdi mdi-clock-outline"></i> <span>3 min ago</span></p>
+                                        <p class="mb-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ultricies fringilla elit in pulvinar</p>
+                                        <p class="small mb-0"><i class="mdi mdi-clock-outline"></i> <span>3 min ago</span></p>
                                     </div>
                                 </div>
                             </div>
@@ -194,23 +181,8 @@
                                 <div class="flex-grow-1">
                                     <h6 class="mb-1">Your item is shipped</h6>
                                     <div class="font-size-13 text-muted">
-                                        <p class="mb-1">If several languages coalesce the grammar</p>
-                                        <p class="mb-0"><i class="mdi mdi-clock-outline"></i> <span>3 min ago</span></p>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-
-                        <a href="#!" class="text-reset notification-item">
-                            <div class="d-flex">
-                                <div class="flex-shrink-0 me-3">
-                                    <img src="{{ asset('assets/images/users/avatar-6.jpg') }}" class="rounded-circle avatar-sm" alt="user-pic">
-                                </div>
-                                <div class="flex-grow-1">
-                                    <h6 class="mb-1">Salena Layfield</h6>
-                                    <div class="font-size-13 text-muted">
-                                        <p class="mb-1">As a skeptical Cambridge friend of mine occidental.</p>
-                                        <p class="mb-0"><i class="mdi mdi-clock-outline"></i> <span>1 hour ago</span></p>
+                                        <p class="mb-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ultricies fringilla elit in pulvinar</p>
+                                        <p class="small mb-0"><i class="mdi mdi-clock-outline"></i> <span>1 days ago</span></p>
                                     </div>
                                 </div>
                             </div>

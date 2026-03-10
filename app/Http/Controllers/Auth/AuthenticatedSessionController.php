770c1c761b412a6821de8a4373705e0e023b2768
Achmad Izaaz <53255893+achmadizaaz@users.noreply.github.com>
2026-03-11 03:48:51 +0700

    Add login history

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\LoginHistory;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -27,6 +28,14 @@ class AuthenticatedSessionController extends Controller
         $request->authenticate();
 
         $request->session()->regenerate();
+
+        LoginHistory::create([
+            'user_id' => auth()->id(),
+            'ip_address' => request()->ip(),
+            'device' => request()->header('User-Agent'),
+            'browser' => request()->header('User-Agent'),
+            'login_at' => now()
+        ]);
 
         return redirect()->intended(route('dashboard', absolute: false));
     }

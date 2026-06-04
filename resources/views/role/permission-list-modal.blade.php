cb0818f3e8af17a7bd86284e61b03bb5079b2d0e
Achmad Izaaz <53255893+achmadizaaz@users.noreply.github.com>
2026-06-04 16:54:23 +0700

    Fix detail permission role

---
+++ resources/views/role/permission-list-modal.blade.php
@@ -0,0 +1,24 @@
+<div class="modal fade profile-modal" id="permissionListModal" tabindex="-1" aria-labelledby="permissionListModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content profile-modal-content">
+            <div class="modal-header profile-modal-header">
+                <div class="modal-heading">
+                    <span class="modal-icon modal-icon-emerald"><i class="bi bi-shield-check"></i></span>
+                    <div>
+                        <h5 class="modal-title" id="permissionListModalLabel">Permission Role</h5>
+                        <p id="permissionListDescription">Daftar permission berdasarkan grup.</p>
+                    </div>
+                </div>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <div class="modal-body profile-modal-body">
+                <div id="permissionGroupList"></div>
+            </div>
+
+            <div class="modal-footer profile-modal-footer">
+                <button class="btn btn-light-modern" type="button" data-bs-dismiss="modal">Tutup</button>
+            </div>
+        </div>
+    </div>
+</div>
